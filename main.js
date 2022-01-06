d6cc328937c1288aef4c8fc70d2f52fa68c6e674
coder309533 <72287735+coder309533@users.noreply.github.com>
2022-01-05 19:27:19 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+
+}
+function setup() {
+    canvas=createCanvas(600,600);
+    canvas.center();
+    video=createCapture(VIDEO);
+}
+function draw() {
+
+}
+function take_snapshot() {
+    save ('mask.png');
+
+}
